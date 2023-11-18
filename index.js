ef3a3b0e936550ed1f766efc7ac1175d04271c14
mahroofpk <mahroofpk@gmail.com>
2023-11-18 08:31:52 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! : V2')
 })
 
 app.get('/status', (req, res) => {
